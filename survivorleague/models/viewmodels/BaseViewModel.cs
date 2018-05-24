4b0a635b11a6f58b66bd7582abcd7cb43f842005
WalterHuetwohl <walter.huetwohl@gmail.com>
2018-05-24 10:52:06 -0500

    Commit BaseViewModel

---
+++ survivorleague/models/viewmodels/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SurvivorLeague.Models.ViewModels
+{
+    public abstract class BaseViewModel
+    {
+        public int PlayerId { get; protected set; }
+        public string PlayerName { get; protected set; }
+
+        public BaseViewModel()
+        {
+            PlayerId = -1;
+            PlayerName = "-1";
+        }
+
+        public BaseViewModel(int playerId, string playerName)
+        {
+            PlayerId = PlayerId;
+            PlayerName = PlayerName;
+        }
+    }
+}
